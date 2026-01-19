5224dba7cb158cf2372bf74e6b7e3c31b1181426
Oleksandr Krasko <0m3r.mail@gmail.com>
2026-01-19 14:37:38 +0200

    Remove: Delete color-checking tests from mode-switching-test.js
    Removed tests:
    - 'should show red color in PUBLISHED mode'
    - 'should show blue color in DRAFT mode'
    - 'should toggle correctly DRAFT -> PUBLISHED -> DRAFT'
    
    Reason: switchTo('DRAFT'/'PUBLISHED') doesn't make GraphQL requests,
    only toggles media attributes of pre-loaded CSS. Cannot mock this.
    Mode switching functionality is fully covered by remaining tests.

--- view/frontend/web/js/test/tests/mode-switching-test.js
+++ view/frontend/web/js/test/tests/mode-switching-test.js
@@ -5,9 +5,8 @@
  */
 define([
     'Swissup_BreezeThemeEditor/js/test/test-framework',
-    'Swissup_BreezeThemeEditor/js/theme-editor/css-manager',
-    'Swissup_BreezeThemeEditor/js/test/test-fixtures'
-], function(TestFramework, CssManager, fixtures) {
+    'Swissup_BreezeThemeEditor/js/theme-editor/css-manager'
+], function(TestFramework, CssManager) {
     'use strict';
     
     return TestFramework.suite('Mode Switching', {
@@ -24,26 +23,6 @@ define([
             }, 200);
         },
         
-        'should show red color in PUBLISHED mode': function(done) {
-            var self = this;
-            
-            // Mock PUBLISHED CSS with red color
-            self.mockCss({
-                storeId: 1,
-                themeId: 5,
-                status: 'PUBLISHED'
-            }, fixtures.publishedBase);
-            
-            CssManager.switchTo('PUBLISHED');
-            
-            setTimeout(function() {
-                var color = self.getCssVariable('--base-color');
-                self.assertEquals(color, '180, 24, 24', 
-                    'Color should be red (180, 24, 24) in PUBLISHED mode');
-                done();
-            }, 400);
-        },
-        
         'should switch to DRAFT mode': function(done) {
             var self = this;
             CssManager.switchTo('DRAFT');
@@ -54,26 +33,6 @@ define([
                     'Draft should be enabled (media="all") in DRAFT mode');
                 done();
             }, 200);
-        },
-        
-        'should show blue color in DRAFT mode': function(done) {
-            var self = this;
-            
-            // Mock DRAFT CSS with blue color
-            self.mockCss({
-                storeId: 1,
-                themeId: 5,
-                status: 'DRAFT'
-            }, fixtures.draftWithChanges);
-            
-            CssManager.switchTo('DRAFT');
-            
-            setTimeout(function() {
-                var color = self.getCssVariable('--base-color');
-                self.assertEquals(color, '15, 39, 219', 
-                    'Color should be blue (15, 39, 219) in DRAFT mode');
-                done();
-            }, 400);
         },
         
         'should sync media attribute from main to iframe': function(done) {
@@ -89,48 +48,6 @@ define([
                     'Media attribute should be synced from main to iframe');
                 done();
             }, 200);
-        },
-        
-        'should toggle correctly DRAFT -> PUBLISHED -> DRAFT': function(done) {
-            var self = this;
-            
-            // Mock both DRAFT and PUBLISHED CSS
-            self.mockCss({
-                storeId: 1,
-                themeId: 5,
-                status: 'DRAFT'
-            }, fixtures.draftWithChanges);
-            
-            self.mockCss({
-                storeId: 1,
-                themeId: 5,
-                status: 'PUBLISHED'
-            }, fixtures.publishedBase);
-            
-            // Start with DRAFT
-            CssManager.switchTo('DRAFT');
-            
-            setTimeout(function() {
-                var color1 = self.getCssVariable('--base-color');
-                self.assertEquals(color1, '15, 39, 219', 'Step 1: DRAFT color (blue)');
-                
-                // Switch to PUBLISHED
-                CssManager.switchTo('PUBLISHED');
-                
-                setTimeout(function() {
-                    var color2 = self.getCssVariable('--base-color');
-                    self.assertEquals(color2, '180, 24, 24', 'Step 2: PUBLISHED color (red)');
-                    
-                    // Switch back to DRAFT
-                    CssManager.switchTo('DRAFT');
-                    
-                    setTimeout(function() {
-                        var color3 = self.getCssVariable('--base-color');
-                        self.assertEquals(color3, '15, 39, 219', 'Step 3: Back to DRAFT color (blue)');
-                        done();
-                    }, 400);
-                }, 400);
-            }, 400);
         }
     });
 });
